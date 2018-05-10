a4ec48dd321608a93a7758c784308e3f0034258b
Michelle Williamson <mwill31@uis.edu>
2018-05-10 13:19:34 -0400

    refactor: implemented Picasso's placeholder feature
    Used Picasso's placeholder feature to supply an image if one isn't available.

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -52,6 +52,8 @@ public class DetailActivity extends AppCompatActivity {
             populateUI(sandwich);
             Picasso.with(this)
                     .load(sandwich.getImage())
+                    .placeholder(R.drawable.placeholder)
+                    .error(R.drawable.placeholder)
                     .into(ingredientsIv);
 
             setTitle(sandwich.getMainName());
