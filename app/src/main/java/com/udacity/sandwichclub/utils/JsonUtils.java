09158ac637415ca924d2da6d184341dd5d371ce3
Michelle Williamson <mwill31@uis.edu>
2018-05-10 13:20:31 -0400

    refactor: abstracted out names of columns to use constants

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -12,18 +12,26 @@ public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) throws JSONException {
 
+        final String KEY_NAME = "name";
+        final String KEY_MAIN_NAME = "mainName";
+        final String KEY_ALSO_KNOWN_AS = "alsoKnownAs";
+        final String KEY_PLACE_OF_ORIGIN = "placeOfOrigin";
+        final String KEY_DESCRIPTION = "description";
+        final String KEY_IMAGE = "image";
+        final String KEY_INGREDIENTS = "ingredients";
+
         Sandwich sandwich = new Sandwich();
 
         try {
             JSONObject jsonSandwich = new JSONObject(json);
 
             // Extract the main name
-            JSONObject name = jsonSandwich.getJSONObject("name");
-            String mainName = name.getString("mainName");
+            JSONObject name = jsonSandwich.getJSONObject(KEY_NAME);
+            String mainName = name.getString(KEY_MAIN_NAME);
             sandwich.setMainName(mainName);
 
             // Extract the alsoKnownAs
-            JSONArray alsoKnownAs = name.getJSONArray("alsoKnownAs");
+            JSONArray alsoKnownAs = name.getJSONArray(KEY_ALSO_KNOWN_AS);
             if (alsoKnownAs != null) {
                 ArrayList<String> aliases = new ArrayList<>();
                 for (int i = 0; i < alsoKnownAs.length(); i++) {
@@ -33,16 +41,16 @@ public class JsonUtils {
             }
 
             // Extract place of origin
-            sandwich.setPlaceOfOrigin(jsonSandwich.getString("placeOfOrigin"));
+            sandwich.setPlaceOfOrigin(jsonSandwich.getString(KEY_PLACE_OF_ORIGIN));
 
             // Extract description
-            sandwich.setDescription(jsonSandwich.getString("description"));
+            sandwich.setDescription(jsonSandwich.getString(KEY_DESCRIPTION));
 
             // Extract image
-            sandwich.setImage(jsonSandwich.getString("image"));
+            sandwich.setImage(jsonSandwich.getString(KEY_IMAGE));
 
             // Extract ingredients
-            JSONArray jsonIngredients = jsonSandwich.getJSONArray("ingredients");
+            JSONArray jsonIngredients = jsonSandwich.getJSONArray(KEY_INGREDIENTS);
             if (jsonIngredients != null) {
                 ArrayList<String> ingredientsArray = new ArrayList<>();
                 for (int i = 0; i < jsonIngredients.length(); i++) {
